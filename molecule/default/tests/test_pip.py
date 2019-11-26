61a83db9dd73a3c43b0614cdb773f82907155f4a
frite <mmavrofides@gmail.com>
2019-11-26 21:58:05 +0200

    Fix indentation

--- molecule/default/tests/test_pip.py
+++ molecule/default/tests/test_pip.py
@@ -9,12 +9,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_pip2_github(host):
     ''' Test that github3.py is installed.'''
-    pip = 'pip'
-    if host.system_info.distribution == 'CentOS' and
-        host.system_info.release > 7:
-            pip = 'pip2'
+    pip_bin = 'pip'
 
-    pip2_path = host.find_command(pip)
+    if host.system_info.distribution == 'CentOS' and host.system_info.release > 7:
+        pip_bin = 'pip2'
+
+    pip2_path = host.find_command(pip_bin)
     packages = host.pip_package.get_packages(pip_path=pip2_path)
 
     assert 'Flask' in packages
