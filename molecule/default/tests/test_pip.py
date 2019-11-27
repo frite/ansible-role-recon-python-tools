a2052d18e52266c1333859e938231c9e947e6382
Manolis M <mmavrofides@gmail.com>
2019-11-27 10:50:26 +0200

    Fix Pip idempotence (#1)
    * Fix Pip idempotence
    
    * Lint
    
    * Fix incorrect
    
    * Fix param
    
    * Wrong assertion
    
    * Minor
    
    * Fix for pip for CentOS 8
    
    * Fix indentation
    
    * Hmm..
    
    * Fix for centos
    
    * Convert to int
    
    * Minor

--- molecule/default/tests/test_pip.py
+++ molecule/default/tests/test_pip.py
@@ -7,17 +7,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_pip2_github(host):
-    ''' Test that github3.py is installed.'''
-    pip_path = host.find_command('pip')
-    packages = host.pip.get_packages(pip_path)
+def test_pip2_flask(host):
+    ''' Test that Flask is installed.'''
+    pip_bin = 'pip'
 
-    assert 'github3.py' in packages
+    if host.system_info.distribution.lower() == 'centos' and \
+            int(host.system_info.release) >= 8:
+        pip_bin = 'pip2'
+
+    pip2_path = host.find_command(pip_bin)
+    packages = host.pip_package.get_packages(pip_path=pip2_path)
+
+    assert 'certifi' in packages
 
 
-def test_test_pip3_github(host):
+def test_test_pip3_flask(host):
     ''' Test that github3.py is installed.'''
     pip3_path = host.find_command('pip3')
-    packages = host.pip.get_packages(pip3_path)
+    packages = host.pip_package.get_packages(pip_path=pip3_path)
 
-    assert 'github3.py' in packages
+    assert 'Flask' in packages
